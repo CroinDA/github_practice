bd186b9d8ace44a737634b2553f5fceeea666410
K.Oliver Park <plks1803@naver.com>
2024-10-02 15:46:20 +0900

    Fix: pull request

--- main2.py
+++ main2.py
@@ -1,7 +1,13 @@
 from typing import Union
 
 def add(x: Union[int, float], y: Union [int, float]) -> Union[int, float]:  # x, y는 정수나 실수로 입력, 출력도 정수나 실수로 출력 
-    return x+y
+    try:
+        result = x+y
+    except Exception as e:
+        print(e)
+        return None
+        
+    return result
 
 if __name__ == "__main__":
     print(add(1, 2))
